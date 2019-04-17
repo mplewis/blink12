3f083e2c176d8e4e1514bc4adacb4f4499b4ccd1
Matt Lewis <matt@mplewis.com>
2019-04-17 13:11:48 -0600

    feat: basic grid

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Grid />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Grid from "./components/Grid";
 
 @Component({
   components: {
-    HelloWorld
+    Grid
   }
 })
 export default class App extends Vue {}
 </script>
+
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+body
+  width 600px
+  margin-left auto
+  margin-right auto
 </style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="grid">
+    <div class="row" v-for="(line, ln) in body" :key="ln">
+      <div class="col" v-for="(char, cn) in line" :key="cn">
+        <span class="char" v-html="char" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+const data = `
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+1234567  01234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+`.trim();
+
+function toChars(line: string) {
+  return Array.from(line.replace(/ /g, "\xa0"));
+}
+
+const body = data.split("\n").map(toChars);
+
+export default {
+  data: () => ({
+    body
+  })
+};
+</script>
+
+<style lang="stylus" scoped>
+@font-face {
+  font-family: 'VCR';
+  src: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/225473/VCR_OSD_MONO_1.001.ttf');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.grid {
+  font-family: 'VCR';
+  font-size: 36px;
+}
+
+.row {
+  height: 28px;
+}
+
+.col {
+  display: inline-block;
+}
+
+.char {
+  display: inline-block;
+}
+</style>
